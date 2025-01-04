ed9bc584b2cd158c834c23138ae86e2c65fbbed3
pml-emerson <efranca@paradisemobile.com>
2025-01-04 18:58:48 -0300

    menuToggle customHook

---
+++ src/hooks/useMenuToggle.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect, useCallback } from 'react';
+
+export const useMenuToggle = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isMenuOpen]);
+
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen(prev => !prev);
+  }, []);
+
+  return {
+    isMenuOpen,
+    toggleMenu,
+  };
+};
