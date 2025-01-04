eab2ff479ba0352f1528eec1e0bbf8c0bfb02be4
pml-emerson <efranca@paradisemobile.com>
2025-01-04 20:12:26 -0300

    tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import './globals.css';
 
 export const metadata: Metadata = {
   title: "Moises",
@@ -13,7 +14,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main style={{ padding: 32 }}>{children}</main>
+        <main className="min-h-screen">
+          {children}
+        </main>
       </body>
     </html>
   );
