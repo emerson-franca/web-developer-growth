20825c3bb9480e59ed43d9455a1bb03e13c90f98
pml-emerson <efranca@paradisemobile.com>
2025-01-05 18:21:31 -0300

    adjust config

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(-50%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        scroll: "scroll 15s linear infinite",
+      },
+      fontFamily: {
+        sans: ["Articulat CF", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
