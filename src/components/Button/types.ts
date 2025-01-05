b38996fb30312f71c261879f536eed1d893550cb
pml-emerson <efranca@paradisemobile.com>
2025-01-05 18:11:39 -0300

    Button type update

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,18 +1,14 @@
+import { Button } from "@/types/index";
+
 export type ButtonAppearance = "solid" | "ghost";
 export type ButtonSize = "sm" | "md" | "lg";
 export type ButtonColor = "blue" | "white";
 
-export interface ButtonProps {
+export type ButtonProps = {
   appearance?: ButtonAppearance;
   size?: ButtonSize;
   color?: ButtonColor;
   href?: string;
-  text: string;
-  subText?: string | null;
-  fluid?: boolean;
-  newTab?: boolean;
-  startIcon?: string | null;
-  endIcon?: string | null;
   className?: string;
-  onClick?: () => void;
-}
+  onClick?: VoidFunction;
+} & Button;
